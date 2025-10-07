af4990f95b7a528071bb93cd4db4ec06a0503ca5
Nasraabdullahi <Naa004@edu.zealand.dk>
2025-10-07 11:42:47 +0200

    Task 3 completed: ændrKlasse function added and tested

--- js/index.js
+++ js/index.js
@@ -34,27 +34,17 @@ const nyUgesynd = lavLi("unhealthy", "5", "Chips");
 tilføjTilListe("listUnhealthy", nyUgesynd);
 
 
+  // Task 3: Skift eller tilføj class-attribut
+function skiftKlasse(element, nyKlasse) {
+    if (element) {
+      element.setAttribute("class", nyKlasse);
+    }
+  }
+  
+  // Test Task 3
+  const liToUpdate = document.getElementById("2"); // Beer
+  skiftKlasse(liToUpdate, "healthy");             // Skifter class til "healthy"
+  
 
 
-//vælg original liste
-//const originalList = document.getElementById("list");
-//vælg den tomme liste under usunde varer
-//const unhealthyList = document.getElementById("unhealthy-list");
-//vælg loop gennem alle li og kopier de usunde varer til den nye liste
-//const items = originalList.getElementsByTagName("li");
 
-//for (let i = 0; i < items.length;i++){
-  //  const item = items[i];
-   // if(item.classList.contains("unhealthy")){
-       // const clone = item.cloneNode(true);
-       // unhealthyList.appendChild(clone);
- //   }
-//}
-//opret et nyt li element
-//const li = document.createElement("li");
-//li.textContent = "Mælk";
-//li.setAttribute("id","item1");
-//li.setAttribute("class","list-item");
-
-//tilføj li elementet til ul
-//ul.appendChild(li);
